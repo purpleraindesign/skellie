7db84fa69a3bb8e0493efe9f7a5425e7a72d100a
Elehisie <elehisie@gmail.com>
2014-08-29 16:42:02 -0300

    further modifications in content

--- config.rb
+++ config.rb
@@ -1,22 +1,23 @@
-# Require any additional compass plugins here.
 require 'font-awesome-sass'
+# Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-fonts_dir = "fonts"
 images_dir = "images"
 javascripts_dir = "scripts"
+fonts_dir = "fonts"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
