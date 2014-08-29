b8dacf61623d90146068de847da8743018190ec4
Elehisie <elehisie@gmail.com>
2014-08-29 15:59:25 -0300

    Adding font-awesome and content

--- config.rb
+++ config.rb
@@ -1,9 +1,11 @@
 # Require any additional compass plugins here.
+require 'font-awesome-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
+fonts_dir = "fonts"
 images_dir = "images"
 javascripts_dir = "scripts"
 
