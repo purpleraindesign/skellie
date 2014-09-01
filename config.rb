1518a74ce773fb0834795b563b008d606d34de85
Elehisie <elehisie@gmail.com>
2014-09-01 11:54:04 -0300

    styling further. switched to a new skin file

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'font-awesome-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "scripts"
